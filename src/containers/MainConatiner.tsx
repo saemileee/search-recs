baf935f621a1fc280c7dc6815e06f17cc2982e37
saem <saemilee01@gmail.com>
2023-09-05 22:04:30 +0900

    feat: JSX 추가

--- src/containers/MainConatiner.tsx
+++ src/containers/MainConatiner.tsx
@@ -9,7 +9,19 @@ const MainContainer = () => {
         };
         getSearchRecs();
     }, []);
-    return <h1>Main</h1>;
+    return (
+        <>
+            <h1>
+                국내 모든 임상시험 검색하고
+                <br />
+                온라인으로 참여하기
+            </h1>
+            <div>
+                <input type='text' />
+                <button>Search</button>
+            </div>
+        </>
+    );
 };
 
 export default MainContainer;
