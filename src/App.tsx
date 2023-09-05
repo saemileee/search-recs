58888ea24facd4406cfae23781a54d6262ada68c
saem <saemilee01@gmail.com>
2023-09-05 22:00:19 +0900

    feat: 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import './App.css';
+import {Outlet} from 'react-router-dom';
 
 const App = () => {
-    return <div className='App'></div>;
+    return <Outlet />;
 };
-
 export default App;
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,1 +1,16 @@
-export {};
+import {createBrowserRouter} from 'react-router-dom';
+import App from './App';
+import {default as Main} from './containers/MainConatiner';
+
+export const Router = createBrowserRouter([
+    {
+        path: '',
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Main />,
+            },
+        ],
+    },
+]);
