421312794ae0f45b833818039ba021db26572958
saem <saemilee01@gmail.com>
2023-09-06 05:00:39 +0900

    refactor: 리턴값 수정

--- src/apis/search.ts
+++ src/apis/search.ts
@@ -1,6 +1,7 @@
 import {searchAPI} from './axiosInstance';
 
 export const getSearchRecs = async (char: string) => {
+    console.info('calling api');
     const res = await searchAPI.get('?q=' + char);
-    return res.data;
+    return res;
 };
