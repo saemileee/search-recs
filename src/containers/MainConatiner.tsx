52dc40b844fecb2f037b0ed4517353cbde8fee82
saem <saemilee01@gmail.com>
2023-09-05 22:27:46 +0900

    feat: 추천 검색어 받아오는 기능

--- src/containers/MainConatiner.tsx
+++ src/containers/MainConatiner.tsx
@@ -1,14 +1,28 @@
-import {useEffect} from 'react';
+import React, {useState} from 'react';
 import * as Fetcher from '../apis/search';
+import * as Type from '../types/searchTypes';
 
 const MainContainer = () => {
-    useEffect(() => {
-        const getSearchRecs = async () => {
-            const data = await Fetcher.getSearchRecs('');
-            console.info(data);
-        };
-        getSearchRecs();
-    }, []);
+    const [searchChar, setSearchChar] = useState('');
+    const [recs, setRecs] = useState<Type.searchRecs>([]);
+    const [isLoading, setIsLoading] = useState(searchChar.length ? true : false);
+
+    const getSearchRecs = async (char: string) => {
+        try {
+            const data = await Fetcher.getSearchRecs(char);
+            setRecs(data);
+        } catch (e) {
+            console.error(e);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const handleTypeKeyword = (char: string) => {
+        setSearchChar(char);
+        getSearchRecs(char);
+    };
+
     return (
         <>
             <h1>
@@ -17,9 +31,20 @@ const MainContainer = () => {
                 온라인으로 참여하기
             </h1>
             <div>
-                <input type='text' />
+                <input
+                    type='text'
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        handleTypeKeyword(e.target.value)
+                    }
+                    placeholder='질환명을 입력해 주세요.'
+                />
                 <button>Search</button>
             </div>
+            <ul>
+                {isLoading && <li>검색 중...</li>}
+                {searchChar.length && <li>{searchChar}</li>}
+                {recs.length && recs.map(data => <li>{data.sickNm}</li>)}
+            </ul>
         </>
     );
 };
