14351967c739e24ed5589aa57b6e739820c2b4fb
saem <saemilee01@gmail.com>
2023-09-07 02:59:17 +0900

    fix: 대괄호 예외처리

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,5 +1,5 @@
 export const isValidKeyword = (string: string) => {
-    const regex = /^(?! )[가-힣A-Za-z\\[\] ]+$/g;
+    const regex = /^(?! )[가-힣A-Za-z]+$/g;
     if (regex.test(string)) {
         return true;
     }
