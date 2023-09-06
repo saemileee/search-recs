cbc62bfc7867c59141190600083fb09f6b7f2c6a
saem <saemilee01@gmail.com>
2023-09-06 22:53:28 +0900

    fix: 디버깅용 코드 추가 및 splice->slice 변경

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -13,13 +13,16 @@ const useSearch = () => {
 
     const getSearchRecs = useCallback(
         (queryKey: string, expireTime: number) => {
+            console.info('패칭 함수 호출 쿼리키: ' + queryKey);
             const nowTime = new Date().getTime();
 
             const fetchData = async () => {
                 try {
+                    console.info('api 통신' + queryKey);
                     const res = await Fetcher.getSearchRecs(queryKey);
-                    const splicedData = res.data.splice(0, MAX_RECS_LENGTH);
-                    setData(splicedData);
+
+                    const slicedData = res.data.slice(0, MAX_RECS_LENGTH);
+                    setData(slicedData);
                     setCache(
                         cache.set(queryKey, {
                             createdAt: nowTime,
@@ -38,15 +41,21 @@ const useSearch = () => {
                 const cached = cache.get(queryKey);
 
                 const {data, createdAt, expireTime} = cached!;
+                console.info('저장된 쿼리키 있음, 캐시된 전체 데이터: ');
+                console.info(queryKey, cached);
 
                 if (nowTime - createdAt < expireTime) {
-                    const splicedData = data.splice(0, MAX_RECS_LENGTH);
-                    setData(splicedData);
+                    console.info('캐시 시간이 남아 저장된 데이터 받아오기');
+
+                    const slicedData = data.slice(0, MAX_RECS_LENGTH);
+                    setData(slicedData);
                     setIsLoading(false);
                 } else {
+                    console.info('시간 만료 돼서 api 호출');
                     fetchData();
                 }
             } else {
+                console.info('쿼리키 없어서 api 호출');
                 fetchData();
             }
         },
