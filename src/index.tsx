0f1af72c5072929daaa5d1e8a78285089027d698
saem <saemilee01@gmail.com>
2023-09-07 15:48:06 +0900

    refactor: 리코일 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {Router} from './Router';
 import {RouterProvider} from 'react-router-dom';
-import {RecoilRoot} from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <RecoilRoot>
-            <RouterProvider router={Router} />
-        </RecoilRoot>
+        <RouterProvider router={Router} />
     </React.StrictMode>
 );
