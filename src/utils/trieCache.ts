12bedff20ce45ae51d7abf9d8a6ed5c2dd36b86b
saem <saemilee01@gmail.com>
2023-09-15 00:48:27 +0900

    refactor: 주석 설명 및 함수 변수 명 변경

--- src/utils/trieCache.ts
+++ src/utils/trieCache.ts
@@ -47,9 +47,9 @@ class Node {
 }
 
 export class TrieCache {
-    root: InterfaceNode;
-    currentNode: InterfaceNode;
-    cacheStorage: InterfaceLocalStorage;
+    private root: InterfaceNode;
+    private currentNode: InterfaceNode;
+    private cacheStorage: InterfaceLocalStorage;
     constructor(cacheStorage: InterfaceLocalStorage) {
         this.cacheStorage = cacheStorage;
         this.root = new Node('');
@@ -105,96 +105,109 @@ export class TrieCache {
     };
 
     private addChild(key: string, node: Node) {
-        // 인자로 받은 Node는 Node{...} 로 들어오기 때문에 스프레드 연산자 써야함
+        // 인자로 받은 Node는 Node{...} 로 들어오기 때문에 스프레드 연산자로 객체 형태로 변환
         this.currentNode.children[key] = {...node};
     }
 
-    private getMostSimilarCache(string: string) {
+    private getCommonPrefixCache(string: string) {
         try {
+            // 가장 먼저 동작하는 메소드이기 때문에 cache를 오픈함
             this.openCache();
             // currentNode를 캐시스토리지에서 가져온 루트로 초기화
             this.currentNode = this.root;
             let mostSimilarNode = this.currentNode;
 
+            // 문자열을 순회하며 노드 체크
             for (let i = 0; i < string.length; i++) {
                 const char = string[i];
+
+                // 현재 노드의 children에 이미 문자열이 저장되어 있는 경우
                 if (char in this.currentNode.children) {
                     this.currentNode = this.currentNode.children[char];
 
-                    const isNeededDeleteData = this.isExpired(this.currentNode);
-                    // 현재 노드의 캐시 만료되었다면 데이터 삭제
-                    if (isNeededDeleteData) {
+                    // 노드 순회 중 만료된 캐시를 본다면 삭제 (가비지 컬렉팅)
+                    const currentCacheIsExpired =
+                        this.currentNode.expireTime !== null && this.isExpired(this.currentNode);
+                    if (currentCacheIsExpired) {
                         this.currentNode = {
                             ...this.currentNode,
                             data: null,
                             expireTime: null,
                             createdAt: null,
                         };
-                        this.cacheStorage.setItem(JSON.stringify(this.root));
+                        this.initCacheStorage();
                     }
 
                     // 현재 노드에 데이터가 있다면 mostSimilarNode 업데이트
+                    // 현재 노드가 있지만 데이터가 없는 경우는 가비지 컬렉팅 당한 경우 혹은 중간 문자열으로 검색이 안된 경우임
+                    // 따라서 문자열 마지막까지 순회하며 mostSimilarNode를 찾아야함
                     if (this.currentNode.data) {
                         mostSimilarNode = this.currentNode;
                     }
                 } else {
-                    // currentNode.children에 현재 문자열이 없다면 현재 노드가 들어온 string 하고 가장 비슷한 노드임
-                    // 데이터가 있는 노드를 반환
+                    // currentNode.children에 현재 문자열이 없다면 현재 노드가 새로 검색한 string 하고 가장 비슷한 노드임
                     return mostSimilarNode;
                 }
             }
 
             return mostSimilarNode;
         } catch (e) {
-            console.info('에러');
-            this.initCacheStorage();
+            console.error('순회가 불가한 구조입니다. 캐시 스토리지를 초기화합니다.');
+            this.resetCacheStorage();
         }
     }
 
-    // 이건 가장 비슷한 캐시를 찾고 다시 인서트할 때 사용함
     insertCache(string: string, cacheInfo: TypeCacheInfo) {
         const {data, expireTime} = cacheInfo;
+
+        // currentNode는 getSimilar를 순회한 후 마지막 노드이기 때문에 startIdx는 비슷한 노드의 value 길이 다음 부터 체크하면 됨
         const startIdx = this.currentNode.value.length;
 
-        // currentNode는 getSimilar 를 순회한 마지막 노드이기 때문에 startIdx는 비슷한 노드의 value 길이 다음부터 체크하면 됨
         for (let i = startIdx; i < string.length; i++) {
             const char = string[i];
-            const charNeedsAddData = i === string.length - 1;
             const newValue = this.currentNode.value + char;
 
+            // 마지막 문자열에 데이터 추가
+            const charNeedsAddData = i === string.length - 1;
             if (charNeedsAddData) {
                 this.addChild(char, new Node(newValue, data, expireTime, this.getCurrentTime()));
             } else {
+                // 마지막 문자열이 아닌 경우 value 값만 있는 노드 추가
                 this.addChild(char, new Node(newValue));
                 this.currentNode = this.currentNode.children[char];
             }
         }
-        this.cacheStorage.setItem(JSON.stringify(this.root));
+
+        // 업데이트 한 root 노드를 cacheStorage에 업데이트
+        this.initCacheStorage();
     }
 
     getCacheData(string: string) {
         const lowerCaseString = string.toLowerCase();
-        const similarNode = this.getMostSimilarCache(lowerCaseString);
-        const similarData = similarNode?.data;
 
-        const similarHasData = similarData;
-        const similarValueIsSameWString = similarNode?.value === lowerCaseString;
+        // 검색한 문자열의 앞 문자열들로 캐싱된 데이터가 있는지 찾기
+        const commonPrefixNode = this.getCommonPrefixCache(lowerCaseString);
+        const commonPrefixData = commonPrefixNode?.data;
 
-        if (similarHasData) {
-            if (similarValueIsSameWString) {
-                return similarData;
+        // 앞 문자열이 같은 노드에 데이터가 있는 경우
+        if (commonPrefixData) {
+            // 앞 문자열이 같은 노드의 value 값이 정확히 일치한다면 바로 해당 데이터 반환
+            const isPrefixEqual = commonPrefixNode?.value === lowerCaseString;
+            if (isPrefixEqual) {
+                return commonPrefixData;
             } else {
-                const newData = similarData.filter(data =>
+                // 아니라면 유사 데이터 중 현재 검색한 값과 일치하는 데이터만 찾아서 재캐싱
+                const newData = commonPrefixData.filter(data =>
                     data.toLowerCase().includes(lowerCaseString)
                 );
-                const cacheInfo = {data: newData, expireTime: similarNode.expireTime};
-
+                const cacheInfo = {data: newData, expireTime: commonPrefixNode.expireTime};
                 this.insertCache(lowerCaseString, cacheInfo);
-
                 return newData;
             }
         }
-        this.currentNode = this.root;
+
+        // 검색 문자열이 아예 새로운 문자열이라면 false 반환
+        // false인 경우 api를 호출하고 insert 메서드 사용하기
         return false;
     }
 }
